d9b6c027f6aa10a3cd23c1dc23b2fe4d3f611bae
Anuken <arnukren@gmail.com>
2026-07-11 10:37:41 -0400

    Re-added server module

---
+++ server/src/steamworks/SteamGameServerAPI.java
@@ -0,0 +1,62 @@
+package steamworks;
+
+public class SteamGameServerAPI{
+
+    public enum ServerMode{
+        Invalid,
+        NoAuthentication,
+        Authentication,
+        AuthenticationAndSecure
+    }
+
+    private static boolean isRunning = false;
+    private static boolean isNativeAPILoaded = false;
+
+
+    public static void loadLibraries() throws SteamException{
+
+        if(isNativeAPILoaded){
+            return;
+        }
+
+        SteamAPI.loadLibraries();
+
+        SteamSharedLibraryLoader.loadLibrary("frogworks-server");
+
+        isNativeAPILoaded = true;
+    }
+
+    public static void skipLoadLibraries(){
+        isNativeAPILoaded = true;
+    }
+
+    public static boolean init(int ip, short gamePort, short queryPort,
+                               ServerMode serverMode, String versionString) throws SteamException{
+
+        if(!isNativeAPILoaded){
+            throw new SteamException("Native server libraries not loaded.\nEnsure to call SteamGameServerAPI.loadLibraries() first!");
+        }
+
+        isRunning = SteamGameServerAPINative.nativeInit(ip, gamePort, queryPort, serverMode.ordinal(), versionString);
+
+        return isRunning;
+    }
+
+    public static void shutdown(){
+        isRunning = false;
+        SteamGameServerAPINative.nativeShutdown();
+    }
+
+    public static void runCallbacks(){
+        SteamGameServerAPINative.runCallbacks();
+    }
+
+    public static boolean isSecure(){
+        return SteamGameServerAPINative.isSecure();
+    }
+
+    public static SteamID getSteamID(){
+        return new SteamID(SteamGameServerAPINative.nativeGetSteamID());
+    }
+
+}
